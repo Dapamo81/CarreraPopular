e66caaa4d1d86c927fc0ceba3998b62407c7ae7d
Dani <dpajaresfo@gmail.com>
2025-09-07 12:43:14 +0200

    Configuramos acceso BBDD

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 require('dotenv').config();
 
-const pool = mysql.createPool({
+const connexion = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
-module.exports = pool;
+connexion.connect((err) => {
+  if (err) {
+    console.error('Error de conexión a la base de datos:', err);
+    return;
+  }else{
+    console.log('Conexión a la base de datos establecida');
+  }
+});
+
+module.exports = connexion;
